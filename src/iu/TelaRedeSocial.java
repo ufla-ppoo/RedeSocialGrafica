5b190dc7c304f080242665ce7d2a0399f51da3e3
Nathan <nathansaraujo191@gmail.com>
2023-12-02 11:44:51 -0300

    ajustando tratamenntto e exceção

--- src/iu/TelaRedeSocial.java
+++ src/iu/TelaRedeSocial.java
@@ -20,45 +20,45 @@ import feed.Publicacao;
 
 /**
  * Classe criada para implementar a interface gráfica da Rede Social.
- * O objetivo dessa implementação é didático! 
- * - Exercitar e apresentar conceitos de GUIs (Interfaces Gráficas de Usuário) 
- *   e Tratamento de Exceções
+ * O objetivo dessa implementação é didático!
+ * - Exercitar e apresentar conceitos de GUIs (Interfaces Gráficas de Usuário)
+ * e Tratamento de Exceções
  * 
  * @author Julio Cesar Alves
  */
 public class TelaRedeSocial {
     // Janela da nossa tela
     private JFrame janela;
-    // Caixa de texto para exibir o feed de noticiai    
-    private JTextArea areaTextoFeed;    
+    // Caixa de texto para exibir o feed de noticiai
+    private JTextArea areaTextoFeed;
     // Botão para postar uma mensagem no feed
     private JButton botaoPostarMensagem;
     // Botão para curtir uma mensagem do feed
     private JButton botaoCurtir;
     // Botão para comentar uma mensagem do feed
     private JButton botaoComentar;
-    
+
     // Objeto que representa a Regra de Negócios (a lógica da Rede Social em si)
     private FeedNoticias feed;
-    
+
     /**
      * Construtor da classe: cria o feed, os componentes e monta a tela.
-    */
+     */
     public TelaRedeSocial() {
         feed = new FeedNoticias();
-        
+
         construirJanela();
     }
 
     /**
      * Constroi os componentes e monta a janela
-    */
+     */
     private void construirJanela() throws HeadlessException {
         janela = new JFrame("GUI - Rede Social");
         janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+
         criarComponentes();
-        
+
         montarJanela();
     }
 
@@ -71,18 +71,18 @@ public class TelaRedeSocial {
         botaoPostarMensagem = new JButton("Postar Mensagem");
         botaoCurtir = new JButton("Curtir");
         botaoComentar = new JButton("Comentar");
-        
+
         // impede que o usuário edite a área de texto do feed
         areaTextoFeed.setEditable(false);
-        
+
         // adiciona o método que tratará o evento de clique no botão Postar Mensagem
         botaoPostarMensagem.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 postarMensagem();
-            }            
+            }
         });
-        
+
         // adiciona o método que tratará o evento de clique no botão Curtir
         botaoCurtir.addActionListener(new ActionListener() {
             @Override
@@ -97,19 +97,19 @@ public class TelaRedeSocial {
      */
     private void montarJanela() {
         janela.setSize(500, 600);
-        
+
         janela.setLayout(new BorderLayout());
 
         JPanel painelSuperior = new JPanel();
         painelSuperior.setLayout(new FlowLayout());
         painelSuperior.add(new JLabel("Feed de Notícias"));
         janela.add(painelSuperior, BorderLayout.NORTH);
-        
+
         JPanel painelCentral = new JPanel();
         painelCentral.setLayout(new BoxLayout(painelCentral, BoxLayout.Y_AXIS));
         painelCentral.add(areaTextoFeed);
         janela.add(painelCentral, BorderLayout.CENTER);
-        
+
         JPanel painelBotoes = new JPanel();
         painelBotoes.setLayout(new FlowLayout());
         painelBotoes.add(botaoPostarMensagem);
@@ -117,14 +117,14 @@ public class TelaRedeSocial {
         painelBotoes.add(botaoComentar);
         janela.add(painelBotoes, BorderLayout.SOUTH);
     }
-    
+
     /*
      * Exibe a tela da Rede Social
-    */
+     */
     public void exibir() {
         janela.setVisible(true);
     }
-    
+
     /**
      * Posta uma mensagem no feed. Solicita o autor e a mensagem ao usuário,
      * posta no Feed e atualiza a área de texto de exibição do feed.
@@ -132,39 +132,46 @@ public class TelaRedeSocial {
     private void postarMensagem() {
         String autor = JOptionPane.showInputDialog("Autor da mensagem");
         // Se o usuário digitou algum autor e confirmou
-        if(autor != null) {
+        if (autor != null) {
             String mensagem = JOptionPane.showInputDialog("Texto da mensagem");
             // Se o usuário digitou alguma mensagem e confirmou
             if (mensagem != null) {
-                feed.postarMensagemTexto(autor, mensagem);        
+                feed.postarMensagemTexto(autor, mensagem);
                 atualizarAreaTextoFeed();
             }
         }
     }
-    
+
     /**
      * Curte uma mensagem. Solicita o identificador da mensagem ao usuário,
      * curte a mensagem e atualiza a área de texto de exibição do feed.
      */
     private void curtirMensagem() {
-        int idMensagem = Integer.parseInt(JOptionPane.showInputDialog("Id da mensagem"));
-        feed.curtir(idMensagem);
-        atualizarAreaTextoFeed();
-    }    
+        try {
+            String idMensagem = JOptionPane.showInputDialog("Id da mensagem");
+            if (!idMensagem.equals("0")) {
+                feed.curtir(Integer.parseInt(idMensagem));
+                atualizarAreaTextoFeed();
+            }
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "O id da mensagem deve ser um número inteiro válido.");
+        }
+    }
 
     /**
      * Atualiza a área de texto de exibição do Feed.
      */
-    private void atualizarAreaTextoFeed() {  
+    private void atualizarAreaTextoFeed() {
         // Limpa a lista de publicações
         areaTextoFeed.setText("");
 
         // Obtém as publicações do feed de notícias
         List<Publicacao> publicacoes = feed.getPublicacoes();
 
-        // Percorre a lista de publicações adicionando na área de texto o texto da publicação
+        // Percorre a lista de publicações adicionando na área de texto o texto da
+        // publicação
         for (Publicacao publicacao : publicacoes) {
             areaTextoFeed.append(publicacao.getTextoExibicao());
         }
-    }   
+    }
 }
